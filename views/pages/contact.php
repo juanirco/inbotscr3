bf7128b7cea53e00ed57a8c0b8906403b7b17929
juanirco <juanirco@hotmail.com>
2025-01-28 19:54:33 -0600

    many changes

--- views/pages/contact.php
+++ views/pages/contact.php
@@ -1,20 +1,57 @@
-<?php include_once __DIR__ . '/header_en.php'?>
 <div class="contact">
     <main class="hero">
         <div class="hero_content">
             <h1>
-                <span>Contact <-> Contact us for any quotation or inquiry</span>
+                <span>Contact <-> Get in touch for any quote or inquiry</span>
             </h1>
         </div>
     </main>
     <section class="section_content">
-        <h1 class="title">
-            <?php echo $title?>
-        </h1>
         <div class="section_div">
-            <p>If you are looking to get info about AI bots, here in Inbotscr we can assist you.</p>
-            <p>This is the place where we address doubts and inquiries.</p>
+            <p>If you're looking for information about Smartbots or AI-powered bots, you've come to the right place.</p>
+            <p>This is where we answer any questions or concerns.</p>
+
+            <h2>Your Choice</h2>
+            <p>You can chat with our smartbot (literally have a conversation, either with voice messages or text) on your preferred channel—and most importantly, get an instant response.</p>
+            
+            <div class="contact_chnls">
+                <div class="contact_chnl">
+                    <a href="https://wa.me/50686324262" target="_blank">
+                        <img src="../build/img/whatsapp.png" alt="WhatsApp icon">
+                        <p>WhatsApp Bot</p>
+                    </a>
+                </div>
+                <div class="contact_chnl">
+                    <a href="https://m.me/inbotscr" target="_blank">
+                        <img src="../build/img/fb.png" alt="Facebook Messenger icon">
+                        <p>Facebook Messenger Bot</p>
+                    </a>
+                </div>
+                <div class="contact_chnl">
+                    <a href="https://ig.me/m/inbotscr" target="_blank">
+                        <img src="../build/img/instagram.png" alt="Instagram icon">
+                        <p>Instagram Bot</p>
+                    </a>
+                </div>
+            </div>
+
+            <h2>Or</h2>
+            <p>You can go the traditional (and boring) way. Subject to availability and business hours (responses may take longer).</p>
+            
+            <div class="contact_chnls">
+                <div class="contact_chnl">
+                    <a href="tel:+17869849940" target="_blank">
+                        <img src="../build/img/telefono_icon.png" alt="Phone icon">
+                        <p>Call us</p>
+                    </a>
+                </div>
+                <div class="contact_chnl">
+                    <a href="mailto:elcorreoquequieres@correo.com" target="_blank">
+                        <img src="../build/img/email.png" alt="Email icon">
+                        <p>Send an Email</p>
+                    </a>
+                </div>
+            </div>
         </div>
     </section>
 </div>
-<?php include_once __DIR__ . '/footer_en.php'?>
